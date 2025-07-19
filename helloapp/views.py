7edfa159f6dbdda2885d3de4c2aa312689131756
GobindPrasad <70059671+da-gobindprasad@users.noreply.github.com>
2025-07-19 12:52:14 +0530

    Added Bootstrap navbar and hello.html template

--- helloapp/views.py
+++ helloapp/views.py
@@ -4,4 +4,4 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def hello(request):
-    return HttpResponse("Hello, world! 👋 from Django")
+    return render(request, 'helloapp/hello.html')
